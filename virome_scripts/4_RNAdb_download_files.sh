4d3f00000dc48c13dee71ca70b913b52d3ec405f
Sydney Birch <152522276+sydney-birch@users.noreply.github.com>
2025-05-27 10:42:25 -0400

    Create 4_RNAdb_download_files.sh

---
+++ virome_scripts/4_RNAdb_download_files.sh
@@ -0,0 +1,5 @@
+#download the following files to create the rRNA db
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_138_1/Exports/SILVA_138.1_LSURef_NR99_tax_silva.fasta.gz
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_138_1/Exports/SILVA_138.1_SSURef_NR99_tax_silva.fasta.gz
+wget https://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/fasta_files/Rfam.fa
+wget https://data.gtdb.ecogenomic.org/releases/release214/214.1/genomic_files_all/ssu_all_r214.tar.gz
